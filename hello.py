021304bd239210c1b2744da35029e198b0a08e13
cai <cccong9597@sina.com>
2016-11-19 17:03:16 +0800

    m

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+say = "CC is awesome"
 @app.route("/")
 def hello():
-    return "Hello World!\n"
+    return "<h1>Hello World!</h1><h2>%s</h2>" % say
+
+@app.route("/change/<word>")
+def change(word):
+    say = word
+    return word
 
 if __name__ == "__main__":
     app.run()
