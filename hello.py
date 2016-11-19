0a1bdcb8e82a1ca0b0979dd52128590c7e1c17fb
cai <cccong9597@sina.com>
2016-11-19 17:29:00 +0800

    global

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+global say 
 say = "CC is awesome"
 @app.route("/")
 def hello():
+    global say
     return "<h1>Hello World!</h1><h2>%s</h2>" % say
 
 @app.route("/change/<word>")
 def change(word):
+    global say
     say = word
     return word
 
